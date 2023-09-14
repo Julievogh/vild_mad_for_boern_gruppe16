21e37ef18f4f3ade498b8e00a9f96b2c01077ed1
Julie Vogh <juvo0001@stud.kea.dk>
2023-09-14 08:47:50 +0200

    opdateret index side

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Nav bar
-
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navLinks = document.querySelector(".nav-links");
 
@@ -15,40 +13,61 @@ window.addEventListener("resize", () => {
   }
 });
 
-// --- CAROUSEL --- //
-const slideContainer = document.querySelector(".slide_container");
-const slides = document.querySelectorAll(".slide");
-const prevButton = document.querySelector(".btn--left");
-const nextButton = document.querySelector(".btn--right");
+// Hent alle faner og deres tilsvarende indholdselementer
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
 
-let currentSlideIndex = 0;
+// Lyt til klik på faner
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    // Skjul eller vis indholdet afhængigt af den aktuelle tilstand
+    if (tabContents[index].classList.contains("active")) {
+      tabContents[index].classList.remove("active");
+    } else {
+      // Skjul alt indhold
+      tabContents.forEach((content) => {
+        content.classList.remove("active");
+      });
 
-// NÆSTE SLIDE
-nextButton.addEventListener("click", () => {
-  if (currentSlideIndex < slides.length - 1) {
-    currentSlideIndex++;
-  } else {
-    currentSlideIndex = 0; // Reset to the first slide
-  }
-  scrollSlides();
-});
-
-// FORRIGE SLIDE
-prevButton.addEventListener("click", () => {
-  if (currentSlideIndex > 0) {
-    currentSlideIndex--;
-  } else {
-    currentSlideIndex = slides.length - 1; // Set to the last slide
-  }
-  scrollSlides();
-});
-
-// CURRENT SLIDE
-function scrollSlides() {
-  const slideWidth = slides[currentSlideIndex].offsetWidth;
-  const scrollX = slideWidth * currentSlideIndex;
-  slideContainer.scrollTo({
-    left: scrollX,
-    behavior: "smooth",
+      // Vis kun det valgte indhold
+      tabContents[index].classList.add("active");
+    }
   });
+});
+
+// Hent alle bokse med klassen "box"
+const boxes = document.querySelectorAll(".box");
+
+// Hent knapperne med ID'erne "prev" og "next"
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+// Initialiser variablen currentIndex til 0
+let currentIndex = 0;
+
+// Funktion til at vise en bestemt boks ved at ændre dens klasse til "active"
+function visBoks(index) {
+  // Fjern "active" klassen fra alle bokse
+  boxes.forEach((box) => box.classList.remove("active"));
+  // Tilføj "active" klassen til den valgte boks
+  boxes[index].classList.add("active");
 }
+
+// Lyt efter klik på "prev" knappen
+prevButton.addEventListener("click", () => {
+  // Beregn det nye index for den foregående boks
+  currentIndex = (currentIndex - 1 + boxes.length) % boxes.length;
+  // Vis den boks med det nye index
+  visBoks(currentIndex);
+});
+
+// Lyt efter klik på "next" knappen
+nextButton.addEventListener("click", () => {
+  // Beregn det nye index for den næste boks
+  currentIndex = (currentIndex + 1) % boxes.length;
+  // Vis den boks med det nye index
+  visBoks(currentIndex);
+});
+
+// Vis den første boks som standard på små skærme
+visBoks(currentIndex);
